312329fe18c5951e9fdeb3a8498206ffcbfb7d82
T3RK0-1101 <2021pjg13@gmail.com>
2025-08-28 08:42:05 -0600

    funcion calcular

--- 3NLIDTS_PabloJimenez_03py/_3NLIDTS_PabloJimenez_03py.py
+++ 3NLIDTS_PabloJimenez_03py/_3NLIDTS_PabloJimenez_03py.py
@@ -1,6 +1,38 @@
 import tkinter as tk
 from tkinter import messagebox
 
+def calcular_temps():
+    if tbCelcius.get() or tbFahrenheit.get() or tbKelvin.get():
+        try:
+            if tbCelcius.get():
+                celsius_val = float(tbCelcius.get())
+                fahrenheit_val = (celsius_val * 9/5) + 32
+                kelvin_val = celsius_val + 273.15
+                tbFahrenheit.delete(0, tk.END)
+                tbFahrenheit.insert(0, str(round(fahrenheit_val, 2)))
+                tbKelvin.delete(0, tk.END)
+                tbKelvin.insert(0, str(round(kelvin_val, 2)))
+            elif tbFahrenheit.get():
+                fahrenheit_val = float(tbFahrenheit.get())
+                celsius_val = (fahrenheit_val - 32) * 5/9
+                kelvin_val = celsius_val + 273.15
+                tbCelcius.delete(0, tk.END)
+                tbCelcius.insert(0, str(round(celsius_val, 2)))
+                tbKelvin.delete(0, tk.END)
+                tbKelvin.insert(0, str(round(kelvin_val, 2)))
+            elif tbKelvin.get():
+                kelvin_val = float(tbKelvin.get())
+                celsius_val = kelvin_val - 273.15
+                fahrenheit_val = (celsius_val * 9/5) + 32
+                tbCelcius.delete(0, tk.END)
+                tbCelcius.insert(0, str(round(celsius_val, 2)))
+                tbFahrenheit.delete(0, tk.END)
+                tbFahrenheit.insert(0, str(round(fahrenheit_val, 2)))
+        except ValueError:
+            messagebox.showerror("Error", "Por favor ingrese valores validos para el calculo.")
+    else:
+        messagebox.showwarning("Advertencia", "Ingrese valores para el calculo de temperatura.")
+
 def limpiar():
     tbCelcius.delete(0, tk.END)
     tbFahrenheit.delete(0, tk.END)
@@ -36,7 +68,7 @@ tbKelvin.grid(row=2, column=1, padx=10, pady=10)
 
 # command es una propiedad de Button que llama a una funcion declarada previamente
 # Botones
-btnCalcular = tk.Button(ventana, text="Calcular") #command=calcular_temps
+btnCalcular = tk.Button(ventana, text="Calcular", command=calcular_temps)
 btnLimpiar = tk.Button(ventana, text="Limpiar", command=limpiar)
 
 btnCalcular.grid(row = 3, column = 0, padx = 10, columnspan = 2, pady = 10)
